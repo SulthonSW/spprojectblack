b750d9a262293cbbef51507c4b43e70f4e744f0e
OEM Configuration (temporary user) <sidiqkeren31@gmail.com>
2021-08-02 15:12:41 +0700

    bis commit

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,10 +16,6 @@ class ApiController extends Controller
     $input->tinggi_sekarang1 = $request->tinggi_sekarang1;
     $input->save();
 }
-    public function spj1_1()
-    {
-     return Proyek1::all();
-    }
     public function cpj1_2(request $request)
     {
         $input = new Proyek1;
@@ -28,10 +24,7 @@ class ApiController extends Controller
         $input->tinggi_sekarang2 = $request->tinggi_sekarang2;
         $input->save();
     }
-    public function spj1_2()
-    {
-     return Proyek1::all();
-    }
+
     public function cpj1_3(request $request)
     {
         $input = new Proyek1;
@@ -40,10 +33,21 @@ class ApiController extends Controller
         $input->tinggi_sekarang3 = $request->tinggi_sekarang3;
         $input->save();
     }
+    public function spj1_2()
+    {
+     return Proyek1::all();
+    }
+    
+    public function spj1_1()
+    {
+     return Proyek1::all();
+    }
+
     public function spj1_3()
     {
      return Proyek1::all();
     }
+    
     public function cpj2_1(request $request)
     {
         $input = new Proyek1;
@@ -52,32 +56,37 @@ class ApiController extends Controller
         $input->tinggi_sekarang1 = $request->tinggi_sekarang1;
         $input->save();
     }
-        public function spj2_1()
-        {
-         return Proyek2::all();
-        }
-        public function cpj2_2(request $request)
-        {
-            $input = new Proyek2;
-            $input->tinggi_awal2 = $request->tinggi_awal2;
-            $input->kedalaman2 = $request->kedalaman2;
-            $input->tinggi_sekarang2 = $request->tinggi_sekarang2;
-            $input->save();
-        }
-        public function spj2_2()
-        {
-         return Proyek2::all();
-        }
-        public function cpj2_3(request $request)
-        {
-            $input = new Proyek2;
-            $input->tinggi_awal3 = $request->tinggi_awal3;
-            $input->kedalaman3 = $request->kedalaman3;
-            $input->tinggi_sekarang3 = $request->tinggi_sekarang3;
-            $input->save();
-        }
-        public function spj2_3()
-        {
-         return Proyek1::all();
-        }
+
+    public function cpj2_2(request $request)
+    {
+        $input = new Proyek2;
+        $input->tinggi_awal2 = $request->tinggi_awal2;
+        $input->kedalaman2 = $request->kedalaman2;
+        $input->tinggi_sekarang2 = $request->tinggi_sekarang2;
+        $input->save();
+    }
+    public function cpj2_3(request $request)
+    {
+        $input = new Proyek2;
+        $input->tinggi_awal3 = $request->tinggi_awal3;
+        $input->kedalaman3 = $request->kedalaman3;
+        $input->tinggi_sekarang3 = $request->tinggi_sekarang3;
+        $input->save();
+    }
+    public function spj2_1()
+    {
+     return Proyek2::all();
+    }
+    
+    public function spj2_2()
+    {
+     return Proyek2::all();
+    }
+    
+    public function spj2_3()
+    {
+     return Proyek1::all();
+    }
+
+        
 }
